df9d7b0955fe1f00fb2701e80b7b875b132e4896
GautamMehta19 <mehtag810@gmail.com>
2022-06-08 00:18:38 +0530

    mongo-part2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const UserModel= require("../models/userModel.js")
+// const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", BookController.createBook )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/allBook", BookController.allBook )
 
-router.post("/createBook", BookController.createBook  )
+router.get("/bookList", BookController.bookList)
 
-router.get("/getBooksData", BookController.getBooksData)
+router.post("/getBooksInYear", BookController.getBooksInYear)
+
+router.post("/getParticularBooks", BookController.getParticularBooks)
+
+router.get("/getXINRBooks", BookController.getXINRBooks)
+
+router.get("/getRandomBooks", BookController.getRandomBooks)
 
 module.exports = router;
