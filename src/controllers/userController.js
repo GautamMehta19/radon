264425d8ef81ca1f25e5fadb2ffa0e927f0ed0d9
GautamMehta19 <mehtag810@gmail.com>
2022-06-11 19:44:04 +0530

    middleware

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,28 +4,34 @@ const UserModel= require("../models/userModel")
 
 
 const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
+    // let tokenDataInHeaders= req.headers.token
+    // console.log(tokenDataInHeaders)
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
+    let date = new Date();
+    let datetime = date.getDate()+" "
+                   +(date.getMonth()+1) + " "
+                   +date.getFullYear()+" "
+                   +date.getHours()+" "
+                   +date.getMinutes()+" "
+                   +date.getSeconds()+" "
+    console.log(datetime);
+
+
+let ip2 = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(
+        list.reduce(
+           (rr, i) => rr.concat
+           (i.family==='IPv4' && !i.internal && i.address || []),
+        [])
+    ),
+[])
+console.log(ip2)
+
+    let  ip = req.ip
+    console.log(ip )
+
+     
     res.send({ msg: "This is coming from controller (handler)"})
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
